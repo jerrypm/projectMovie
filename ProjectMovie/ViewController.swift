a73e85d3030bf849e23945c7a167b7be7266db30
jeri purnama maulid <jerrypurnamamaulid@gmail.com>
2020-05-09 23:57:08 +0700

    Initial Commit

---
+++ ProjectMovie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectMovie
+//
+//  Created by Jerry Purnama Maulid on 09/05/20.
+//  Copyright © 2020 Jerry Purnama Maulid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
